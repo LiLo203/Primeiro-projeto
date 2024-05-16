b8dab739bd92fdc8e3cf0e76da86d86b8edfe768
Murilo Martim <murilolilo590@gmail.com>
2024-05-16 20:18:44 -0300

    adicionando variável

---
+++ Desafio2.js
@@ -0,0 +1,16 @@
+let DiaSemana = prompt('Que dia é hoje?');
+
+if (DiaSemana === 'Sábado' || DiaSemana === 'Domingo') { 
+   alert('Bom fim de semana!');
+} else { if(DiaSemana === 'Segunda','Terça')
+   alert('Boa semana!');
+}
+let PositOuNegat = prompt('Diga um número')
+if (PositOuNegat <=0) {
+
+ alert('Você está negativado!')}
+   else{
+      alert("Você está positivado!")
+   }
+ 
+ 
